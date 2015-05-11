3a97768026efa4c0b7833c9de8d4a5656eff26f5
april284377458 <28437748@qq.com>
2015-05-11 20:24:46 +0800

    5月11

--- admin/code/book.php
+++ admin/code/book.php
@@ -1,30 +1,97 @@
 <?php 
-define('IN_ECS', true); 
-require_once dirname(__FILE__).'/../../commons/init.php';  
-$act = $_REQUEST[act]; 
-if (empty($act)) { 
+define('IN_ECS', true);
+
+//require_once '/totoro/commons/init.php';
+
+require_once dirname(__FILE__).'/../../commons/init.php';
+//require_once '../commons/init.php';
+
+$act = $_REQUEST[act];
+
+if (empty($act)) {
 	$act = "";	
 }
 
-if ($act == "list") { 
-	$users = $db->get_page("sys_book a,sys_booktype b"," where a.booktype_code = b.booktype_code ");  
+if ($act == "list") {
+
+	
+	$arr = array(); 
+	$arr['sys_book_no'] = $_REQUEST['sys_book_no'];
+	$arr['booktype_code'] = $_REQUEST['booktype_code'];
+	
+	$where = " where isdelete = 0 ";
+	foreach ($arr as $ks=>$vs){
+		if(!empty($vs)){
+			$where.= "and $ks = '$vs'";
+		} 
+	}
+	
+	$users = $db->get_page("sys_book",$where); 
 	echo json_encode($users); 
 }elseif ($act == "add") {
 	
+}elseif ($act == "comboxlist") { 
+	$sql=" SELECT sys_booktype_code 'id',sys_booktype_name 'text',sys_booktype_desc 'desc'
+			 from sys_booktype where isdelete = 0 ";
+	$rs = mysql_query($sql);
+	
+	//$items = array();
+	$items = array(0=> array("id"=>"","text"=>"请选择","selected"=>true,"desc"=>""));
+	while($row = mysql_fetch_object($rs)){
+		array_push($items, $row);
+	}  
+	echo json_encode($items); 
 }elseif ($act == "save") {
+	$arr = array();
+	$arr['sys_book_no'] = $_REQUEST['sys_book_no'];
+	$arr['sys_book_value'] = $_REQUEST['sys_book_value'];
+	$arr['booktype_code'] = $_REQUEST['booktype_code'];
+	$arr['czdate'] = "sysdate";
 	
+	try {
+		$db->insert("sys_book",$arr);
+		$result->result="1";
+		$result->msg="添加成功。";
+		
+	}catch (Exception $e){
+		$result->result="0";
+		$result->msg="添加失败。";
+	}
+	echo json_encode($result);	
 }elseif ($act == "update") {
+	$arr = array();
+	$arr['sys_book_no'] = $_REQUEST['sys_book_no'];
+	$arr['sys_book_value'] = $_REQUEST['sys_book_value'];
+	$arr['booktype_code'] = $_REQUEST['booktype_code'];
+	$arr['czdate'] = "sysdate";	
+	$id = $_REQUEST['id'];
 	
-}elseif ($act == "del") {
+	try {
+		$db->update("sys_book",$arr,"where id=".$id);
+		$result->result="1";
+		$result->msg="修改成功。";
+	}catch (Exception $e){
+		$result->result="0";
+		$result->msg="修改失败。";
+	}
+	echo json_encode($result);  
+}elseif ($act == "delete") { 
+	$arr = array();	
+	$id = $_REQUEST['id'];
+	$arr['isdelete'] = 1; 
 	
-}else {
-	try {   
-		  $smarty->display("admin/code/book.html"); 
-	} catch (Exception $e) {   
-		print $e->getMessage();  
-	}   
-}
-?>
-	
+	try {
+		$db->update("sys_book",$arr,"where id=".$id);
+		$result->result="1";
+		$result->msg="删除成功。";
+	}catch (Exception $e){
+		$result->result="0";
+		$result->msg="删除失败。";
+	}
 	 
+	echo json_encode($result);	
+}else {
+	$smarty->display("admin/code/book.html");
+}
 
+?>
